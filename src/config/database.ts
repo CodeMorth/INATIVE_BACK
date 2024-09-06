f094811097c01a5d089f41dcd85cb76594773894
CodeMorth <kevinsalinasvd@gmail.com>
2024-09-06 10:55:02 -0500

    change send mailer message

--- src/config/database.ts
+++ src/config/database.ts
@@ -8,19 +8,19 @@ dotenv.config()
 const { DB_DATABASE, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT } =
   process.env as unknown as DBConfig
 
-  const InnativeDB = new Sequelize(
-    DB_DATABASE, // database name
-    DB_USERNAME, // username
-    DB_PASSWORD, // password
-    {
-      host: DB_HOST,
-      port: parseInt(DB_PORT, 10), // Convierte el puerto a número
-      dialect: 'mysql',
-      logging:false,
-      dialectOptions:{ssl:{require:true,rejectUnauthorized:false}}
+const InnativeDB = new Sequelize(
+  DB_DATABASE, // database name
+  DB_USERNAME, // username
+  DB_PASSWORD, // password
+  {
+    host: DB_HOST,
+    port: DB_PORT, // port should be a number, not a string
+    dialect: 'mysql',
 
-    }
-  )
-  
+  }
+)
 
 export default InnativeDB
+
+    // logging:false,
+    // dialectOptions:{ssl:{require:true,rejectUnauthorized:false}}
--- src/utils/sendMailer.ts
+++ src/utils/sendMailer.ts
@@ -8,13 +8,10 @@ export const sendMail = (email: any, subject: any, username: any) => {
     to: email,
     subject,
     html: `<div style="background:#2B3146; padding: 10px; width: 90%; max-width: 650px; margin: 0 auto;border-radius: 5px; font-family: 'Black Ops One', Arial, sans-serif;">
-      <div style="width: 100%; display: grid; place-items: center;">
-        <img src="${loggo}" alt="logo_dev_friend" style="width: 200px; height: 150px; margin:0 auto;" />
-        
+      <div style="width: 100%; display: grid; place-items: center;">        
         <h4 style="color: #94849C; font-size: 18px; text-align: center;">
-          <span style="color: #F969AA; font-size: 22px;">${username}</span> Bienvenido  a <span style="color: #F969AA; font-size: 22px;">Dev</span> <span style="color: #57A3E1;font-size: 22px;">Friend</span>, donde podrás unificar todas tus tareas con tu equipo.
-        </h4>
-        
+          <span style="color: #F969AA; font-size: 22px;">${username}</span> Bienvenido  a Inative, donde podrás aprender nuevos idiomas con personas reales.
+        </h4>        
       </div>
     </div>`,
   });
